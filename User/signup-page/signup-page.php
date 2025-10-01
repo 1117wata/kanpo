6b3867384410aa1c3789bce1e52e993386775347
Yamaguchi0412 <2401113@s.asojuku.ac.jp>
2025-10-01 11:42:51 +0900

    新規登録画面

--- User/signup-page/signup-page.php
+++ User/signup-page/signup-page.php
@@ -1,17 +1,26 @@
-<!DOCTYPE html>
-<html lang="en">
+<!DOCTYPE html> 
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>新規登録画面</title>
 </head>
 <body>
-    <h1>ログイン</h1>
-    <p>会員の方は、登録時に入力されたメールアドレスとパスワードでログインしてください。</p>
-    メールアドレス
-    <input type="text" name="email">
-    パスワード
-    <input type="password" name="password">
-    <button type="submit">ログイン</button>
+    <h1>新規会員登録</h1>
+    お名前<br>
+    <input type="text" name="name"><br>
+    ※苗字と名前の間にスペースは不要です<br>
+    ニックネーム<br>
+    <input type="text" name="nickname"><br>
+    メールアドレス<br>
+    <input type="email" name="email"><br>
+    パスワード<br>
+    <input type="password" name="password"><br>
+    住所<br>
+    <input type="text" name="address"><br>
+    <br>
+    <button type="submit">戻る</button>
+    <br><br>
+    <button type="submit">この内容で会員登録する</button>
 </body>
 </html>
