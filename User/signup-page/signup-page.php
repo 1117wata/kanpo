d0fe923452ee769eb5c29036ca86b38a158e1502
2kazuki9 <2501912@s.asojuku.ac.jp>
2025-10-01 11:31:43 +0900

    a

--- User/signup-page/signup-page.php
+++ User/signup-page/signup-page.php
@@ -6,12 +6,6 @@
     <title>新規登録画面</title>
 </head>
 <body>
-    <h1>ログイン</h1>
-    <p>会員の方は、登録時に入力されたメールアドレスとパスワードでログインしてください。</p>
-    メールアドレス
-    <input type="text" name="email">
-    パスワード
-    <input type="password" name="password">
-    <button type="submit">ログイン</button>
+    
 </body>
 </html>
