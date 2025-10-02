25d6c6fe0c95681b6dec03dea10aef94e3aa7b7c
shunei0911 <2301010@s.asojuku.ac.jp>
2025-10-02 09:37:33 +0900

    login

--- User/login-page/login.php
+++ User/login-page/login.php
@@ -4,14 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ログイン画面</title>
+    <link rel="stylesheet" href="login.css">
 </head>
 <body>
-    <h1>ログイン</h1>
+    <header></header>
+
+    <img src
     <p>会員の方は、登録時に入力されたメールアドレスとパスワードでログインしてください。</p>
     メールアドレス
-    <input type="text" name="email">
+    <input type="text" name="email"><br>
     パスワード
-    <input type="password" name="password">
+    <input type="password" name="password"><br>
     <button type="submit">ログイン</button>
 </body>
 </html>
