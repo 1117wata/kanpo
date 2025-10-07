bb869778cdc0e2c74016dec1d2924114df4feaf4
2kazuki9 <2501912@s.asojuku.ac.jp>
2025-10-07 12:35:16 +0900

    login.php完了
    後で確認

--- User/login-page/login.php
+++ User/login-page/login.php
@@ -1,26 +1,34 @@
 <?php
 session_start();
-$pdo = new PDO('mysql:host=localhost;dbname=kanpo', 'root', '');
 
-$email = $_POST['email'];
-$password = $_POST['password'];
+$pdo = new PDO('mysql:host=localhost;dbname=kanpo;charset=utf8mb4', 'root', '', [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+]);
 
-$stmt = $pdo->prepare("SELECT * FROM user WHERE email = ? AND password = ?");
-$stmt->execute([$email, $password]);
-$user = $stmt->fetch();
+$error = '';
 
-if ($user) {
-    $_SESSION['user_id'] = $user['id'];
-    $_SESSION['email'] = $user['email'];
-    header("Location: home.php");
-    exit();
-} else {
-    $error = "メールアドレスまたはパスワードが間違っています。";
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $email = $_POST['email'] ?? '';
+    $password = $_POST['password'] ?? '';
+
+    $stmt = $pdo->prepare("SELECT * FROM `user` WHERE email = ?");
+    $stmt->execute([$email]);
+    $user = $stmt->fetch();
+
+    if ($user && password_verify($password, $user['password'])) {
+        $_SESSION['user_id'] = $user['id'];
+        $_SESSION['email'] = $user['email'];
+        header("Location: home.php");
+        exit();
+    } else {
+        $error = "メールアドレスまたはパスワードが間違っています。";
+    }
 }
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -32,27 +40,27 @@ if ($user) {
 
     <img src="../../images/Kanpo.png" class="Kanpo_picture" alt="サンプル画像">
 
-    <form action="home.php" method="post">
+    <form action="" method="post">
         <h1>ログイン</h1>
 
         <p>会員の方は、登録時に入力されたメールアドレスとパスワードでログインしてください。</p>
 
-        <?php if (!empty($error)): ?>
+        <?php if ($error): ?>
             <p style="color:red;"><?php echo $error; ?></p>
         <?php endif; ?>
         
         <div class="message">メールアドレス</div><br>
-        <input type="text" name="email" class="input"><br>
+        <input type="text" name="email" class="input" required><br>
 
         <div class="message">パスワード</div><br>
-        <input type="password" name="password" class="input"><br>
+        <input type="password" name="password" class="input" required><br>
 
         <p><button type="submit" class="button">ログイン</button></p>
     </form>
 
     <hr>
 
-    <form action="../signup-page/signup-page.php" method="get">
+    <form action="../signup-page/signup-page.php" method="post">
         <h3>まだ会員登録されてない方</h3>
         <p>初めてご利用される方は、こちらから会員登録すると便利にご利用できます。</p>
         <p><button type="submit" class="button">新規会員登録</button></p>
