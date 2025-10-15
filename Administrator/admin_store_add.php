2b7a553143f95ec58824b7b46c308964d834a015
Yamaguchi0412 <2401113@s.asojuku.ac.jp>
2025-10-15 11:21:27 +0900

    修正

---
+++ Administrator/admin_store_add.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>店舗追加画面</title>
+    <link rel="stylesheet" href="css/stores.css">
+</head>
+<body>
+    <div class="header-bar">
+    <a href="admin_home.php" class="logo-link">
+        <img src="../images/Akanpo.png" alt="サイトロゴ">
+    </a>
+    <div class="page-title">店舗追加</div>
+</div>
+</body>
+</html>
