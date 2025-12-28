24d6b0e6884911082123fd5740ced909aae771b8
1117wata <2401187@s.asojuku.ac.jp>
2025-12-28 23:08:12 +0900

    Update check_nickname.php

--- User/signup-page/check_nickname.php
+++ User/signup-page/check_nickname.php
@@ -8,7 +8,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['nickname'])) {
     }
 
     try {
-        $pdo = new PDO("mysql:host=localhost;dbname=kanpo;charset=utf8", "root", "");
+        include '../kanpo/DB/db_connect.php';
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
         $stmt = $pdo->prepare("SELECT COUNT(*) FROM user WHERE nickname = :nickname");
